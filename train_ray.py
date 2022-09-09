f0c07c5b7ee8da220d5dcf0e42fc41ca279ffb22
Igor Tokarev <realpolyzer@yandex.ru>
2022-09-10 00:49:47 +0300

    ray train fixed

--- train_ray.py
+++ train_ray.py
@@ -9,24 +9,19 @@ env = NetworkIntrusionEnv
 
 # ray.init()
 # Configure the algorithm.
+num_workers = 10
+rollout = 1000
 config = {
     "env": env,
-    # Use 2 environment workers (aka "rollout workers") that parallelly
-    # collect samples from their own environment clone(s).
-    "num_workers": 5,
-    # Change this to "framework: torch", if you are using PyTorch.
-    # Also, use "framework: tf2" for tf2.x eager execution.
-    "framework": "tf",
-    # Tweak the default model provided automatically by RLlib,
-    # given the environment's observation- and action spaces.
+    "num_workers": num_workers,
+    # "num_gpus": 1.0,
+    "rollout_fragment_length": rollout,
+    "train_batch_size": num_workers*rollout,
+    "framework": "torch",
     "model": {
-        "fcnet_hiddens": [64, 64],
+        "fcnet_hiddens": [128, 128],
         "fcnet_activation": "relu",
     },
-    # Set up a separate evaluation worker set for the
-    # `algo.evaluate()` call after training (see below).
-    "evaluation_num_workers": 1,
-    # Only for evaluation runs, render the env.
 }
 
 stop_dict = {
